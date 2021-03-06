d4c0a355b30ad2be8a6f2c74fc7945fef96a6f2d
DeniseLuiz <denisesantos.luiz@usp.br>
2021-03-05 22:13:45 -0300

    configuração do db para os filtros

--- src/database/test.js
+++ src/database/test.js
@@ -29,5 +29,28 @@ database.then(async(db) => {
         }
     ]
 
-    await createProffy(db, { proffyValue, classValue, classScheduleValues })
+    // await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
+    // Consultar todos os Proffys
+    const selectProffys = await db.all("SELECT * FROM proffys")
+        // console.log(selectProffys)
+
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*                             
+        FROM proffys 
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+        // console.log(selectClassesAndProffys)
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520"
+    `)
+
+    console.log(selectClassesSchedules)
 })
